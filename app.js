ac632356a31bab3a7dfda221e92145b8bf1a996f
Otger Peidro <opeidro@dectechsolutions.com>
2017-06-14 17:53:50 +0200

    Commit del dia 6 minuto 72

--- app.js
+++ app.js
@@ -49,13 +49,24 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+
+  if (isAPI(req)) {
+    res.json({success: false, error: err.message});
+    return;
+  }
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
   res.render('error');
 });
+
+
+function isAPI(req) {
+  return req.originalUrl.indexOf('/apiv') === 0;
+}
+
 
 module.exports = app;
