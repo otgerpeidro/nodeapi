bcdaf273e798527ece7c5b4380ed64aeec05ec7a
Otger Peidro Cid <otgerpeidro@MacBook-Pro-de-Otger-Peidro-Cid.local>
2017-06-15 20:00:48 +0200

    Commit en el minuto 100 de la sesion 1 del dia 6

--- models/Agente.js
+++ models/Agente.js
@@ -10,11 +10,22 @@ const agenteSchema = mongoose.Schema({
         type: String,
         index: true,
         unique: true
-    }
-})
+    },
+    dateofBirth: Date
+});
+
+// creamos un método estático
+agenteSchema.statics.list = function(filter, limit, skip, fields, sort, callback) {
+   const query = Agente.find(filter);
+   query.limit(limit);
+   query.skip(skip);
+   query.select(fields);
+   query.sort(sort);
+   query.exec(callback);
+};
 
 // luego creamos el modelo
-const Agente = mongoose.model('Agente', agenteSchema);
+var Agente = mongoose.model('Agente', agenteSchema);
 
 // Realmente no es necesario exportarlo, ya que en otros sitios 
 //podriamos recuperar el modelo usando : mongoose.model('Agente') 
--- routes/apiv1/agentes.js
+++ routes/apiv1/agentes.js
@@ -2,14 +2,30 @@
 
 var express = require('express');
 var router = express.Router();
-const Agente = requier('../../models/Agente');
+const Agente = require('../../models/Agente');
 // no seria necesario requerir el modulo de Agente ya que podriamos recuperar con 
 // mongoose.model('Agente')
 
 /* GET /apiv1/agentes */
 router.get('/', function(req, res, next) {
     
-    Agente.find().exec((err, agentes) => {
+    const name = req.query.name;
+    const age = req.query.age;
+
+    // creo el filtro vacío
+    const filter = {};
+
+    if (name) {
+        filter.name = name;
+    }
+
+    if (age) {
+        filter.age = age;
+    }
+
+
+    Agente.list(filter, null, null, null, null, (err, agentes) => {
+
         if (err) {
             next(err); // le decimos a express que devuelva el error
             return;
@@ -19,6 +35,23 @@ router.get('/', function(req, res, next) {
     });
     
 });
+
+// POST /apiv1/agentes
+
+router.post('/', (req, res,next) => {
+    console.log(req.body);
+    // creamos un objeto de tipo Agente
+    const agente = new Agente(req.body);
+    // lo guardamos en la base de datos
+    agente.save((err, agenteGuardado) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.json({ success: true, result: agenteGuardado});
+    });
+});
+
 
 
 module.exports = router;
